67b0ebd5054f3ba1f1fb629cd65a48f7095f1a2a
karim <haddadi.karim20@gmail.com>
2024-02-26 13:31:33 +0100

    Update API URLs and parameters

--- src/services/website.js
+++ src/services/website.js
@@ -8,7 +8,19 @@ import instance from "./api.js";
 export async function WebsiteGetAll() {
 //@todo : gestion fine des erreurs 
 
-const response = await instance.get("/websites");
+const response = await instance.get('/website/', {
+    
+    params: {
+        offset: "0", // offset permet de commencer de la ligne 0
+        limit: "150",
+        where:"",
+        
+    },
+    });
+console.log(response.data);
+console.log(response);
+console.log(response.data.list);
+
 return response.data;
 
 
