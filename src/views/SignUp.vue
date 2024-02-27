716f47412718c6b2547a02a992caa4a0538c3e1d
karim <haddadi.karim20@gmail.com>
2024-02-27 11:34:43 +0100

    Refactor SignUp.vue component layout and styles

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,7 +1,6 @@
-<!-- SignView.vue -->
-
 <template>
-
+  <div class="signup-page">
+    <div class="content-container">
     <div class="signup-container">
       <h2>Sign Up</h2>
   
@@ -17,23 +16,48 @@
         <input type="submit" value="Submit">
       </form> 
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        fname: '',
-        lname: '',
-        email: '',
-        pwd: ''
-      }
-    },
-    methods: {
-      submitForm() {
-        // Handle form submission here
-        // You can access form data using this.fname, this.lname, this.email, and this.pwd
-      }
+    <img src="@/assets/signup.png" alt="img">
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fname: '',
+      lname: '',
+      email: '',
+      pwd: ''
+    }
+  },
+  methods: {
+    submitForm() {
+      // Handle form submission here
+      // You can access form data using this.fname, this.lname, this.email, and this.pwd
     }
   }
-  </script>
+}
+</script>
+
+<style scoped>
+.signup-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  gap: 50px;
+  background-color:  #BEC4D7;
+
+}
+
+.content-container {
+  display: flex;
+  gap: 20px; /* Adjust as needed */
+  background-color: #ffffff; /* Adjust as needed */
+  padding: 20px; /* Adjust as needed */
+  border-radius: 10px; /* Adjust as needed */
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Adjust as needed */
+}
+
+</style>
