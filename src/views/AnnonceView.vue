7d68a11ac69a3c8605d32d7d2dda91943e9a122d
karim <haddadi.karim20@gmail.com>
2024-03-02 18:59:16 +0100

    Remove commented out code in AnnonceView.vue

--- src/views/AnnonceView.vue
+++ src/views/AnnonceView.vue
@@ -2,7 +2,6 @@
 import { WebsiteGetAll } from "@/services/website.js";
 import { ref, onMounted } from "vue";
 import NavBar from "@/components/AnnonceNav.vue";
-// import SearchBar from "@/components/SearchBar.vue";
 import Formulaire from "@/components/Formulaire.vue";
 
 const websites = ref([]);
