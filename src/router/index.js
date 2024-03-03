1edf2db4ac9922c2ee8cc56e58ec4bafd95d86ca
karim <haddadi.karim20@gmail.com>
2024-03-03 20:27:54 +0100

    Refactor router imports and formatting

--- src/router/index.js
+++ src/router/index.js
@@ -1,70 +1,59 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
-import AnnonceView from '../views/AnnonceView.vue'
-import CandidacyView from '../views/CandidacyView.vue'
-import ListView from '../views/ListView.vue'
-import CoverView from '../views/CoverView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
+import AnnonceView from "../views/AnnonceView.vue";
+import CandidacyView from "../views/CandidacyView.vue";
+import ListView from "../views/ListView.vue";
+import CoverView from "../views/CoverView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [ 
+  routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-
-
-
-
-    {
-      path: '/signup',
-      name: 'signup',
-      component: () => import('../views/SignUp.vue')
-    },
-
-
-    {
-
-      path: '/login',
-      name: 'login',
-      component: LoginView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
 
     {
-
-      path: '/annonce',
-      name: 'annonce',
-      component: AnnonceView
+      path: "/signup",
+      name: "signup",
+      component: () => import("../views/SignUp.vue"),
     },
 
     {
-
-      path: '/candidacy',
-      name: 'candidacy',
-      component: CandidacyView
+      path: "/login",
+      name: "login",
+      component: LoginView,
     },
 
     {
-
-      path: '/list',
-      name: 'list',
-      component: ListView
+      path: "/annonce",
+      name: "annonce",
+      component: AnnonceView,
     },
 
     {
-
-      path: '/cover',
-      name: 'cover',
-      component: CoverView
+      path: "/candidacy",
+      name: "candidacy",
+      component: CandidacyView,
     },
 
+    {
+      path: "/list",
+      name: "list",
+      component: ListView,
+    },
 
-  ]
-})
+    {
+      path: "/cover",
+      name: "cover",
+      component: CoverView,
+    },
+  ],
+});
 
-export default router
+export default router;
 
-// dans cette page en importe juste les écrans ça veut dire les pages de notre application 
+// dans cette page en importe juste les écrans ça veut dire les pages de notre application
 // on n'importe pas les composants ici
